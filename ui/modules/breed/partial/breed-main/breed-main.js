406205ac67d11d7c85e11e565078212839c016a1
Anton Sotirov <anton.k.sotirov@gmail.com>
2018-11-03 14:08:22 +0200

    moved ui and node-tests in folder front-js - structured outline around tech

--- ui/modules/breed/partial/breed-main/breed-main.js
+++
@@ -1,4 +0,0 @@
-angular.module('breed').controller('BreedMainCtrl',function($scope){
-
-
-});
--- ui/templates/directive/simple/directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('<%= appname %>').directive('<%= _.camelize(name) %>', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs, fn) {
-
-
-        }
-    };
-});
